4f2411b4f7b277fc6f99d3704817d7269fe09d7b
kelvintrc <kvynmutwiri@gmail.com>
2022-07-18 17:05:50 +0000

    [Updated]: Updated 0-bubblesort.c handle time complexity if items are not swappped

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -13,13 +13,13 @@ void bubble_sort(int *array, size_t size)
 	for (i = 0; i < size; i++)
 	{
 		swapped = false;
-		for (j = 1; j < size - i; j++)
+		for (j = 0; j < size - i - 1; j++)
 		{
-			if (array[j - 1] > array[j])
+			if (array[j] > array[j + 1])
 			{
 				temp = array[j];
-				array[j] = array[j - 1];
-				array[j - 1] = temp;
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
 				swapped = true;
 				print_array(array, size);
 			}
