4e09dbab70794ef78855eac996d928544a0784cc
kelvintrc <kvynmutwiri@gmail.com>
2022-07-19 09:41:04 +0000

    [Update]: A function that sorts an array of integers in ascending order using the Selection sort algorithm

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,4 +1,5 @@
 #include "sort.h"
+#include <stdbool.h>
 
 /**
  * selection_sort - sorts an array of integers in
@@ -11,19 +12,30 @@
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j, temp, min_idx;
+	bool swapped;
 
-	/* One by one move boundary of unsorted subarray */
-	for (i = 0; i < size - 1; i++)
+	if (array)
 	{
-		/* Find the minimum element in unsorted array */
-		min_idx = i;
-		for (j = i + 1; j < size; j++)
-			if (array[j] < array[min_idx])
-				min_idx = j;
-		/* Swap the found minimum element with the first element*/
-		temp = array[min_idx];
-		array[min_idx] = array[i];
-		array[i] = temp;
-		print_array(array, size);
+		/* One by one move boundary of unsorted subarray */
+		for (i = 0; i < size - 1; i++)
+		{
+			/* Find the minimum element in unsorted array */
+			min_idx = i;
+			swapped = false;
+			for (j = i + 1; j < size; j++)
+				if (array[j] < array[min_idx])
+				{
+					min_idx = j;
+					swapped = true;
+				}
+			/* Swap the found minimum element with the first element*/
+			if (swapped == true)
+			{
+				temp = array[min_idx];
+				array[min_idx] = array[i];
+				array[i] = temp;
+				print_array(array, size);
+			}
+		}
 	}
 }
